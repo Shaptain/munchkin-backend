016921cb17f7a07d3fd9540cd5757bf007ce87a6
Shaptain <sama23aiml@cmrit.ac.in>
2025-08-07 20:55:25 +0530

    man

--- server/index.js
+++ server/index.js
@@ -29,9 +29,12 @@ app.get("/messages", (req, res) => {
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
+  cors: {
+    origin: "https://munchkin-frontend-sv4h.vercel.app", 
+    methods: ["GET", "POST"],
+    credentials: true, 
+  },
 });
-
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
